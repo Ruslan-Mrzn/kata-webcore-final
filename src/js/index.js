20b6f4c125c4f58d7d2545b44af517dbac60a7d2
Ruslan <russl.murzin@gmail.com>
2024-05-01 13:09:24 +0900

    feat: add menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 import '../scss/style.scss'
+import { toggleActiveLink, showMore } from './utils'
 
-console.log('Works!')
+toggleActiveLink('nav-list', 'hero__header')
+toggleActiveLink('menu__lang')
+toggleActiveLink('services')
+showMore('hero__text-block', 'hero__overflow')
+
+const menuButton = document.querySelector('.nav-button')
+const menu = document.querySelector('.menu')
+const screen = document.querySelector('.screen')
+const burger = document.querySelector('.burger')
+menuButton.addEventListener('click', (evt) => {
+  evt.preventDefault()
+  menuButton.classList.toggle('nav-button--state--menu-opened')
+  menu.classList.toggle('menu--state--opened')
+  screen.classList.toggle('screen--state--menu-opened')
+  burger.classList.toggle('burger--state--menu-opened')
+})
---
+++ src/js/utils.js
@@ -0,0 +1,52 @@
+const closeMenu = () => {
+  const menuButton = document.querySelector('.nav-button')
+  const menu = document.querySelector('.menu')
+  const screen = document.querySelector('.screen')
+  const burger = document.querySelector('.burger')
+  menuButton.classList.remove('nav-button--state--menu-opened')
+  menu.classList.remove('menu--state--opened')
+  screen.classList.remove('screen--state--menu-opened')
+  burger.classList.remove('burger--state--menu-opened')
+}
+
+const toggleActiveLink = (blockClassName, headerClassName) => {
+  if (headerClassName) {
+    const header = document.querySelector(`.${headerClassName}`)
+    header.textContent = document
+      .querySelector(`.${blockClassName}`)
+      .querySelector('.nav-link--state--active').textContent
+  }
+  const listOfLinks = document
+    .querySelector(`.${blockClassName}`)
+    .querySelectorAll('.nav-link')
+  listOfLinks.forEach((link) => {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault()
+      if (!evt.target.classList.contains('.nav-link--state--active')) {
+        const currentActiveLink = document
+          .querySelector(`.${blockClassName}`)
+          .querySelector('.nav-link--state--active')
+        currentActiveLink.classList.remove('nav-link--state--active')
+        evt.target.classList.add('nav-link--state--active')
+        if (headerClassName) {
+          document.querySelector(`.${headerClassName}`).textContent =
+            evt.target.textContent
+          closeMenu()
+        }
+      }
+    })
+  })
+}
+
+const showMore = (blockClassName, overflowBlockClassName) => {
+  const block = document.querySelector(`.${blockClassName}`)
+  const overflowBlock = block.querySelector(`.${overflowBlockClassName}`)
+  const button = block.querySelector('.show-more-button')
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    overflowBlock.classList.toggle(`${overflowBlockClassName}--state--opened`)
+    button.classList.toggle('show-more-button--state--show')
+  })
+}
+
+export { toggleActiveLink, showMore }
