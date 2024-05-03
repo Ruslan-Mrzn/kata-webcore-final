da3c8f9ed6a5d56471e45351dfa8440ff1b59f1e
Ruslan <russl.murzin@gmail.com>
2024-05-03 22:22:45 +0900

    feat: add slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,41 @@
+import Swiper from 'swiper'
+import { Pagination } from 'swiper/modules'
+import 'swiper/swiper.scss'
+import 'swiper/modules/pagination.scss'
 import '../scss/style.scss'
+
 import { toggleActiveLink, showMore } from './utils'
+
+console.log(window.screen.width)
+if (window.screen.width < 500) {
+  new Swiper('.brands', {
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    modules: [Pagination],
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  })
+  new Swiper('.appliances', {
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    modules: [Pagination],
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  })
+  new Swiper('.prices', {
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    modules: [Pagination],
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  })
+}
 
 toggleActiveLink('nav-list', 'hero__header')
 toggleActiveLink('menu__lang')
